6b2b10a419900df6f1995fa82995219f612f7ecf
Adam <vaebmail@gmail.com>
2019-01-31 12:25:02 +0000

    leaderboard user

--- src/commands/exp/leaderboard.js
+++ src/commands/exp/leaderboard.js
@@ -3,16 +3,27 @@ import { sendEmbed, sendEmbedError, roleRank } from '../../util';
 import { dataMembersAll } from '../../db';
 import { expRoleGuilds, getRankFromXp } from '../../expRoles';
 // import { requiresExpRoles } from '../../permissions';
-// import { userResolvable } from '../../paramTypes';
+import { userResolvable } from '../../paramTypes';
 
 export default {
     cmds: ['leaderboard', 'xpleaderboard', 'xpboard', 'top', 'xptop', 'scores'],
     desc: "Check the server's top XP rankings",
-    params: [],
+    // params: [],
+    params: [
+        {
+            name: 'User',
+            desc: 'User to check',
+            types: userResolvable.types,
+            examples: userResolvable.examples,
+            parse: userResolvable.parse,
+            parseFail: userResolvable.parseFail,
+            optional: true,
+        },
+    ],
 
     // checkPermissions: [requiresExpRoles],
 
-    func: ({ guild, channel, speaker }) => {
+    func: ({ guild, channel, speaker, args }) => {
         const hasRanks = expRoleGuilds.includes(guild.id);
 
         // if (!expRoleGuilds.includes(guild.id)) {
@@ -22,9 +33,25 @@ export default {
 
         if (!expEnabled) return sendEmbed(channel, null, 'XP is temporarily disabled for feature testing');
 
-        const dataMembers = Object.values(dataMembersAll[guild.id])
+        let dataMembers = Object.values(dataMembersAll[guild.id])
             .sort(({ exp: exp1 }, { exp: exp2 }) => exp2 - exp1)
-            .slice(0, 10);
+            .forEach((obj, index) => {
+                obj.pos = index + 1;
+            });
+
+        if (!args[0]) {
+            dataMembers = dataMembers.slice(0, 10);
+        } else {
+            const memberIndex = dataMembers.findIndex(({ userId }) => userId === args[0].id);
+
+            if (memberIndex === -1) return sendEmbedError(channel, 'User not found in the database');
+
+            let cutStart = Math.max(memberIndex - 4, 0);
+            const cutEnd = Math.min(cutStart + 10, dataMembers.length);
+            cutStart = Math.max(Math.min(cutStart, cutEnd - 10), 0);
+
+            dataMembers = dataMembers.slice(cutStart, cutEnd);
+        }
 
         // console.log('dataMembers', dataMembers);
 
@@ -32,8 +59,8 @@ export default {
             channel,
             `${guild.name} XP Ranks`,
             `${noChar}\n${dataMembers
-                .map(({ userId, exp }, index) =>
-                    `[${index + 1}] ${guild.members.get(userId) || `User Left (${userId})`}: ${exp} XP (${
+                .map(({ userId, exp, pos }) =>
+                    `[${pos}] ${guild.members.get(userId) || `User Left (${userId})`}: ${exp} XP (${
                         hasRanks ? getRankFromXp(exp).name : roleRank(guild, userId)
                     })`)
                 .join('\n\n')}`,
